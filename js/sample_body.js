1692d14096557e020582cefea7536abc62352500
ishimins-tss <k-ishikawa@tss-ipnet.com>
2017-06-27 17:04:16 +0900

    CSS JS_body Change

--- js/sample_body.js
+++ js/sample_body.js
@@ -1,14 +1,14 @@
 //ƒJƒŒƒ“ƒ_[•`‰æ—p
-document.write("<table class='cl1'>");
+document.write("<table class='tablemain'>");
 
-document.write("<tr class='cl2'><td colspan='7'>" + year + "”N" + (month + 1) + "Œ</td></tr>");
+document.write("<tr class='titles'><td colspan='7'>" + year + "”N" + (month + 1) + "Œ</td></tr>");
 
-document.write("<tr class='cl5'>"); //—j“ús‚Ìˆ—
+document.write("<tr class='titleLine'>"); //—j“ús‚Ìˆ—
 for(i=0; i<7; i++) {
     if(i == 0){ //“ú—j“ú‚Ì‚Æ‚«
-    document.write("<td class='cl3w'"); //“ú—j“ú—p‚Ì”wŒiFˆ—
+    document.write("<td class='calSunTit'"); //“ú—j“ú—p‚Ì”wŒiFˆ—
     } else if(i == 6) {
-    document.write("<td class='cl4w'"); //“y—j“ú—p‚Ì”wŒiFˆ—
+    document.write("<td class='calSatTit'"); //“y—j“ú—p‚Ì”wŒiFˆ—
     } else { //‚»‚êˆÈŠOi•½“új
     document.write("<td");
     }
@@ -21,11 +21,11 @@ for(i=0; i<cldLines; i++) { //s”‚É‰‚¶‚Ä
     for(j=0; j<7; j++) { //—ñ”=7
         newDate = cldTable[j + (i * 7)];
         if(newDate == date) { //¡“ú‚Ì‚Æ‚«
-            document.write("<td class='cl6'>");
+            document.write("<td class='calToday'>");
         } else if (j == 0) { //“ú—j“ú‚Ì‚Æ‚«
-            document.write("<td class='cl3'>");
+            document.write("<td class='calSun'>");
         } else if (j == 6) { //“y—j“ú‚Ì‚Æ‚«
-            document.write("<td class='cl4'>");
+            document.write("<td class='calSat'>");
         } else {
             document.write("<td>");
         }
