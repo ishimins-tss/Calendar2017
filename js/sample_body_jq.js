92c681dcc160a5766342512dfb34886f15be23f6
ishimins-tss <k-ishikawa@tss-ipnet.com>
2017-07-03 17:21:07 +0900

    jQuery Add 2

--- js/sample_body_jq.js
+++ js/sample_body_jq.js
@@ -1,65 +1,79 @@
-$(document).ready(
+$(document).ready(function(){
 
   $('body').append($("<table>", {
+  href: "index.html",
   target: "_blank",
   "class": "tablemain"
   }));
 
-    $('table').append($("<tr class='titles'><td colspan='7'>" + year + "”N" + (month + 1) + "ŒŽ</td></tr>", {
-    "class": "titles"
+  $('table').append($("<tr>", {
+  "class": "titles"
+  }));
+
+    $('.titles').append($("<td>", {
+    "colspan" : 7 ,
+    "class" : "titleYM"
     }));
 
-    $('table').append($("<tr>", {
-    "class": "titleLine"
-    }));
+      $('.titleYM').append(year + "”N" + (month + 1) + "ŒŽ");
 
-      for(i=0; i<7; i++) {
-        if(i == 0){
-          $('.titleLine').append($("<td>", {
-          "class": "calSunTit"
-          }));
-        } else if (i == 6) {
-          $('.titleLine').append($("<td>", {
-          "class": "calSatTit"
-          }));
-        } else {
-          $('.titleLine').append($("<td>", {
-          "class": "calNonTit"
-          }));
-        }
-        $('.titleLine').append("</td>");
-      }
-      $('table').append("</tr>");
+    $('.titles').append($("</td>"));
 
-    for(i=0; i<cldLines; i++) {
-      $('table').append($("<tr>", {
-      "class" : "calDayIndex"
+  $('table').append($("</tr>"));
+
+  $('table').append($("<tr>", { 
+  "class": "titleLine"
+  }));
+
+  for(i=0; i<7; i++) {
+    if(i == 0){
+      $('.titleLine').append($("<td>", {
+      "class": "calSunTit"
       }));
-      
-      for(j=0; j<7; j++) {
-        newDate = cldTable[j + (i * 7)];
-        if(newDate == date) {
-          $('.calDayIndex').append($("<td>", {
-          "class" : "calToday"
-          }));
-        } else if (j == 0) {
-          $('.calDayIndex').append($("<td>", {
-          "class" : "calSun"
-          }));
-        } else if (j == 6) {
-          $('.calDayIndex').append($("<td>", {
-          "class" : "calSat"
-          }));
-        } else {
-          $('.calDayIndex').append($("<td>", {
-          "class" : "calNone"
-          }));
-        }
-        $('.calDayIndex').append(newDate);
-        
-        $('.calDayIndex').append("</td>");
-      }
-      $('table').append("</tr>");
+    } else if (i == 6) {
+      $('.titleLine').append($("<td>", {
+      "class": "calSatTit"
+      }));
+    } else {
+      $('.titleLine').append($("<td>", {
+      "class": "calNonTit"
+      }));
     }
-    $('body').append("</table>");
-);
+    $('.titleLine').append(newWeekdayTable[i]);
+    
+    $('.titleLine').append($("</td>"));
+  }
+  $('table').append($("</tr>"));
+
+  for(i=0; i<cldLines; i++) {
+    $('table').append($("<tr>", {
+    "class" : "calDayIndex"
+    }));
+
+    for(j=0; j<7; j++) {
+      newDate = cldTable[j + (i * 7)];
+      if(newDate == date) {
+        $('.calDayIndex').append($("<td>", {
+        "class" : "calToday"
+        }));
+      } else if (j == 0) {
+        $('.calDayIndex').append($("<td>", {
+        "class" : "calSun"
+        }));
+      } else if (j == 6) {
+        $('.calDayIndex').append($("<td>", {
+        "class" : "calSat"
+        }));
+      } else {
+        $('.calDayIndex').append($("<td>", {
+        "class" : "calNone"
+        }));
+      }
+      $('.calDayIndex').append(newDate);
+
+      $('.calDayIndex').append($("</td>"));
+    }
+    $('table').append($("</tr>"));
+  }
+  $('body').append($("</table>"));
+});
